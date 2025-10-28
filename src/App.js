a217345839aa65c8eab0b7950e667b101032b45a
unknown <vedatrayani.kosuri@gmail.com>
2025-10-28 12:16:27 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const [message, setMessage] = useState("Loading...");
+ useEffect(() => {
+ fetch("hhttps://test-mern-render-backend-wouf.onrender.com")
+ .then((res) => {
+ if (!res.ok) throw new Error("Network response was not ok");
+ return res.json();
+ })
+ .then((data) => setMessage(data.message))
+ .catch((err) => setMessage("Error: " + err.message));
+ }, []);
+ return (
+ <div style={{ textAlign: "center", marginTop: "50px" }}>
+ <h1>{message}</h1>
+ </div>
+ );
 }
-
 export default App;
